d1352d29aa079c7b20ee19d3e3b824821f152657
imnamgi <happynj2697@gmail.com>
2024-12-22 19:51:16 +0900

    [남기]Feat: 상품리스트 & 장바구니 구현

--- src/router/root.jsx
+++ src/router/root.jsx
@@ -8,7 +8,10 @@ const MainPage = lazy(() => import("../pages/UserPages/MainPage"))
 
 const SignupPage = lazy(() => import("../pages/UserPages/SignupPage"))
 
-const ProductPage = lazy(() => import("../pages/ProductPage"))
+const ProductPage = lazy(() => import("../pages/ProductPage")) // 이거 삭제 해줘
+
+const ProductListPage = lazy(() => import("../pages/product/ProductListPage"));
+const ProductDetailPage = lazy(() => import("../pages/product/ProductDetailPage"));
 
 const root = createBrowserRouter([
     // 12-22 [희원] Feat: MainPage = 로그인(메인), SignupPage = 회원가입
@@ -21,12 +24,24 @@ const root = createBrowserRouter([
         element: <Suspense fallback={Loading}><SignupPage/></Suspense>
     },
 
-    // 테스트 product 페이지 후에 변경
-    {
-        path: 'product',
-        element: <Suspense fallback={Loading}><ProductPage/></Suspense>
-    }
+    // 테스트 product 페이지 후에 변경 // 이거 삭제 해줘
+    {// 이거 삭제 해줘
+        path: 'product',// 이거 삭제 해줘
+        element: <Suspense fallback={Loading}><ProductPage/></Suspense> // 이거 삭제 해줘
+    },// 이거 삭제 해줘
 
+    // 상품 ##########################################################################
+    // 상품 리스트 페이지
+    {
+        path: "product",
+        element: <Suspense fallback={Loading}><ProductListPage /></Suspense>,
+    },
+    // 상품 상세 페이지
+    {
+        path: "product/:productId",
+        element: <Suspense fallback={Loading}><ProductDetailPage /></Suspense>,
+    },
+    // 상품 ##########################################################################
 
 
 ])
