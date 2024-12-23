670b66585989f3f607f14f621e61841b3e66120d
namhui <knaem.edu@gmail.com>
2024-12-23 19:30:11 +0900

    [남희] Fix: resolve conflict

--- src/router/root.jsx
+++ src/router/root.jsx
@@ -1,5 +1,5 @@
 import { Suspense, lazy } from "react";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 import Layout from "../pages/Layout";
 
 const ProductListPage = lazy(() => import("../pages/product/ProductListPage"));
@@ -12,13 +12,29 @@ const LoginPage = lazy(() => import("../pages/user/LoginPage"));
 const SignupPage = lazy(() => import("../pages/user/SignupPage"));
 const MyPage = lazy(() => import("../pages/user/MyPage"));
 
+// 12.23 [남희]
+// Admin Pages
+const AdminMain = lazy(() => import("../pages/adminPage/AdminMain"));
+const MenuManagement = lazy(() => import("../pages/adminPage/MenuManagement"));
+
+// Root Layout 컴포넌트
+const RootLayout = () => {
+  return (
+    <div className="container-fluid">
+      <main className="p-4">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
 const root = createBrowserRouter([
   {
     // 루트 라우트
     path: "/",
     // 가장 바깥을 Layout으로 감싸고,
     // 그 안에 자식 라우트(children)로 각 페이지를 매핑합니다.
-    element: <Layout />,
+    element: <RootLayout />,
     children: [
       {
         // index 라우트 (= "/") 
@@ -64,6 +80,15 @@ const root = createBrowserRouter([
             <MyPage />
           </Suspense>
         ),
+      },
+      // 관리자 페이지
+      {
+        path: "admin",
+        element: <Suspense fallback={Loading}><AdminMain /></Suspense>,
+      },
+      {
+          path: "admin/menu",
+          element: <Suspense fallback={Loading}><MenuManagement /></Suspense>,
       },
     ],
   },
