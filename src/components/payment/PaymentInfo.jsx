847f6852818c6ba5fbb1c1c99f7f36083c8a275b
KimJiwon <rkl45687@gmail.com>
2024-12-25 00:14:42 +0900

    [지원] fix: css 수정, feat: 계좌 복사 기능 추가 12.25

--- src/components/payment/PaymentInfo.jsx
+++ src/components/payment/PaymentInfo.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import './PaymentInfo.css'
+import styles from './PaymentInfo.module.css'
 
 const PaymentInfo = ({cart}) => {
   const [totalPrice, setTotalPrice ]= useState(0)
@@ -10,16 +10,16 @@ const PaymentInfo = ({cart}) => {
   }, [cart]);
   return (
     <>
-      <h3 className='text-center text-jasscoffee mb-3'>주문 현황</h3>
+      <h3 className={`${styles.paymentTextJass} text-center mb-3`}>주문 현황</h3>
       {/* cart 정보 render */}
-      <div className='product-card py-4 px-5 d-flex flex-column justify-content-between'>
+      <div className={`${styles.paymentInfo} py-4 px-5 d-flex flex-column justify-content-between`}>
         <div>
           {// 상품 정보 반복
             cart.map((product, productIdx) => (
               <div key={`product${productIdx}`} className='mb-3 d-flex flex-column'>
                 <span className='fs-4'>{product.productName} - {product.totalPrice}원</span>
                 {product.selectedOptions.length > 0 && ( // 상품정보가 있을 경우에만 보이기
-                  <div className="product-options fs-5">
+                  <div className={`${styles.paymentInfoOptions} fs-5`}>
                     <span className="me-2">상품 옵션 :</span>
                     {// 옵션 정보 반복
                       product.selectedOptions.map((option, optionIdx) => (
